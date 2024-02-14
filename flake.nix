0eda226ffab527bb7cf91df631860af6e305e2e0
Tim Nelson <tim@eltimn.com>
2024-02-13 23:44:24 -0600

    The rest of my dotfiles and packages from the common ansible role

--- flake.nix
+++ flake.nix
@@ -14,16 +14,19 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      username = "nelly";
     in {
-      homeConfigurations."nelly" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      homeConfigurations."${username}" =
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+          extraSpecialArgs = { inherit username; };
+        };
     };
 }
--- home.nix
+++ home.nix
@@ -1,18 +1,9 @@
-{ config, pkgs, ... }:
-
-{
+{ config, pkgs, username, ... }: {
   home = {
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
-    username = "nelly";
-    homeDirectory = "/home/nelly";
-
-    # Packages that should be installed to the user profile.
-    packages = with pkgs; [
-      htop
-      neovim
-      git
-    ];
+    username = "${username}";
+    homeDirectory = "/home/${username}";
 
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
@@ -24,17 +15,48 @@
     # changes in each release.
     stateVersion = "23.11";
 
-    sessionPath = [
-      "$HOME/.config/zsh/scripts"
-      "$HOME/.pulumi/bin"
+    # Packages that should be installed to the user profile.
+    packages = with pkgs; [
+      # ack-grep
+      curl
+      # dnsutils
+      git
+      htop
+      # libnss3-tools
+      libnotify
+      neovim
+      # net-tools
+      nixfmt
+      notify-osd
+      sshfs
+      # trash-cli
+      tree
+      vim
+      xclip
     ];
 
+    # List of extra paths to include in the user profile.
+    sessionPath = [ "$HOME/.pulumi/bin" ];
+
+    # List of environment variables.
     sessionVariables = {
       EDITOR = "/usr/bin/code --new-window --wait";
+      JAVA_HOME = "/usr/lib/jvm/java-17-openjdk-amd64";
     };
 
-    file.".config/Code/User/settings.json".source = ./files/Code/User/settings.json;
-    file.".config/git/config".source = ./files/git/config;
+    # List of files to be symlinked into the user home directory.
+    file.".config/Code/User/settings.json".source =
+      ./files/.config/Code/User/settings.json;
+    file.".config/git/config".source = ./files/.config/git/config;
+    file.".oh-my-zsh-custom".source = ./files/.oh-my-zsh-custom;
+    file.".oh-my-zsh-custom".recursive = true;
+    file."bin".source = ./files/bin;
+    # file."bin".recursive = true;
+    file.".ackrc".source = ./files/.ackrc;
+    file.".ansible.cfg".source = ./files/.ansible.cfg;
+    file.".gitignore".source = ./files/.gitignore;
+    file.".mongoshrc.js".source = ./files/.mongoshrc.js;
+    file.".abcde.conf".source = ./files/.abcde.conf;
   };
 
   programs = {
@@ -44,21 +66,15 @@
     # zsh
     zsh = {
       enable = true;
-      # enableCompletion = true;
-      # enableAutosuggestions = true;
-      # syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      syntaxHighlighting.enable = true;
 
       oh-my-zsh = {
         enable = true;
-        # enableUnstable = true;
-        # theme = "powerlevel10k/powerlevel10k";
-        theme = "robbyrussell";
+        theme = "philips";
         custom = "$HOME/.oh-my-zsh-custom";
-        plugins = [
-          "copyfile"
-          "copypath"
-          "colorize"
-        ];
+        plugins = [ "copyfile" "copypath" "colorize" ];
       };
     };
 
@@ -70,11 +86,11 @@
     };
   };
 
-  services = {
-    gpg-agent = {
-      enable = true;
-      defaultCacheTtl = 1800;
-      enableSshSupport = true;
-    };
-  };
+  # services = {
+  #   gpg-agent = {
+  #     enable = true;
+  #     defaultCacheTtl = 1800;
+  #     enableSshSupport = true;
+  #   };
+  # };
 }
